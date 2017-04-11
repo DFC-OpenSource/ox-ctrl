d30c7e763df0e026cfb629e9bb6a9b31f344a899
ivpi <ivpi@itu.dk>
2017-04-11 05:36:15 +0200

    volt: more slots for DMA

--- mmgr/volt/volt.c
+++ mmgr/volt/volt.c
@@ -11,7 +11,7 @@
 static atomic_t         nextprp;
 static pthread_mutex_t  prp_mutex;
 static pthread_mutex_t  prpmap_mutex;
-static uint64_t         prp_map;
+static uint64_t         prp_map[8];
 
 static VoltCtrl             *volt;
 static struct nvm_mmgr      volt_mmgr;
@@ -23,7 +23,7 @@ static int volt_start_prp_map(void)
     nextprp.counter = ATOMIC_INIT_RUNTIME(0);
     pthread_mutex_init (&prp_mutex, NULL);
     pthread_mutex_init (&prpmap_mutex, NULL);
-    prp_map = 0x0 & AND64;
+    memset (prp_map, 0x0, sizeof(uint64_t) * 8);
 
     return 0;
 }
@@ -31,9 +31,9 @@ static int volt_start_prp_map(void)
 static void volt_set_prp_map(uint64_t index, uint8_t flag)
 {
     pthread_mutex_lock(&prpmap_mutex);
-    prp_map = (flag)
-            ? prp_map | (1 << (index - 1))
-            : prp_map ^ (1 << (index - 1));
+    prp_map[(index - 1) / 64] = (flag)
+            ? prp_map[(index - 1) / 64] | (1 << ((index - 1) % 64))
+            : prp_map[(index - 1) / 64] ^ (1 << ((index - 1) % 64));
     pthread_mutex_unlock(&prpmap_mutex);
 }
 
@@ -53,7 +53,7 @@ static uint8_t volt_get_next_prp(struct volt_dma *dma){
         pthread_mutex_unlock (&prp_mutex);
 
         pthread_mutex_lock(&prpmap_mutex);
-        if (prp_map & (1 << (next - 1))) {
+        if (prp_map[(next - 1) / 64] & (1 << ((next - 1) % 64))) {
             pthread_mutex_unlock(&prpmap_mutex);
             usleep(1);
             continue;
